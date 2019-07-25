5c4e3bba2caf8bc7cc65d908388944fa653d324c
DiGregorioC <digregorioc@outlook.com>
2019-07-25 10:02:17 -0400

    Update db location

--- src/apiConfig.js
+++ src/apiConfig.js
@@ -1,6 +1,6 @@
 let apiUrl
 const apiUrls = {
-  production: 'https://protected-chamber-86780.herokuapp.com/',
+  production: 'https://protected-chamber-86780.herokuapp.com',
   development: 'http://localhost:4741'
 }
 
